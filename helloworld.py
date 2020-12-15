6e3a97b1c8eec215a7f1f399c41a200df477b957
GilErick <72312438+GilErick@users.noreply.github.com>
2020-12-15 00:49:46 -0600

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,6 @@
-# Author: Adolfo Centeno
-# Date: Oct 3, 2020
-# Universidad Veracruzana
+# Author: Erick Gil
+# Date: Nov 14, 2020
+# Centro de Investigaciones Cerebrales
 # helloworld.py
 
 print ("Hello world python")
