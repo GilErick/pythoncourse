e16506603e59a67734d551b1c6f863e273fd3e30
GilErick <72312438+GilErick@users.noreply.github.com>
2020-12-15 01:04:46 -0600

    Delete helloworld.py

--- helloworld.py
+++
@@ -1,8 +0,0 @@
-# Author: Erick Gil
-# Date: Nov 14, 2020
-# Centro de Investigaciones Cerebrales
-# helloworld.py
-
-print ("Hello world python")
-print ("Hello universe ...")
-print ("Welcome to python !!")
